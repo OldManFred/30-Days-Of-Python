aae379a1025fae1af34a48678d910a58effeb299
OldManFred <dl7utx@gmx.de>
2026-09-06 20:46:31 +0200

    Loops

--- Own_Code/30_Days/10_Loops.py
+++ Own_Code/30_Days/10_Loops.py
@@ -1,6 +1,13 @@
 import sys
 import os
-
+#Pfad zu 'data' zur Path Variable hinzufügen
+Pfad=__file__ #__file__ enthält relativer oder absoluter Pfad zum Script
+Pfad=os.path.abspath(Pfad) #in absoluten Pfad wandeln
+DirPfad=os.path.dirname(Pfad)#Filenamen abtrennen
+ParentDirPfad=os.path.join(DirPfad,'..','..')#Zwei Verzeichnisebenen nach oben anhängen
+ParentDirPath=os.path.normpath(ParentDirPfad)#evtl in Normpfad wandeln, nicht unb. nötig
+print(f'Pfad: {ParentDirPath}')
+sys.path.append(ParentDirPath)# Pfad nun in Path aufnehmen
 a,b,c,d=0,1,0,0
 fibo=[0,1]#  Liste erzeugen
 while c<500:
@@ -102,205 +109,10 @@ c=10
 for a in range(1,c):
     b+=b*a
 print(f'{c}! = {b}')
-countries = [
-  'Afghanistan',
-  'Albania',
-  'Algeria',
-  'Andorra',
-  'Angola',
-  'Antigua and Barbuda',
-  'Argentina',
-  'Armenia',
-  'Australia',
-  'Austria',
-  'Azerbaijan',
-  'Bahamas',
-  'Bahrain',
-  'Bangladesh',
-  'Barbados',
-  'Belarus',
-  'Belgium',
-  'Belize',
-  'Benin',
-  'Bhutan',
-  'Bolivia',
-  'Bosnia and Herzegovina',
-  'Botswana',
-  'Brazil',
-  'Brunei',
-  'Bulgaria',
-  'Burkina Faso',
-  'Burundi',
-  'Cabo Verde',
-  'Cambodia',
-  'Cameroon',
-  'Canada',
-  'Central African Republic',
-  'Chad',
-  'Chile',
-  'China',
-  'Colombia',
-  'Comoros',
-  'Congo, Democratic Republic of the',
-  'Congo, Republic of the',
-  'Costa Rica',
-  "Côte d'Ivoire",
-  'Croatia',
-  'Cuba',
-  'Cyprus',
-  'Czech Republic',
-  'Denmark',
-  'Djibouti',
-  'Dominica',
-  'Dominican Republic',
-  'East Timor (Timor-Leste)',
-  'Ecuador',
-  'Egypt',
-  'El Salvador',
-  'Equatorial Guinea',
-  'Eritrea',
-  'Estonia',
-  'Eswatini',
-  'Ethiopia',
-  'Fiji',
-  'Finland',
-  'France',
-  'Gabon',
-  'Gambia',
-  'Georgia',
-  'Deutschland',
-  'Ghana',
-  'Greece',
-  'Grenada',
-  'Guatemala',
-  'Guinea',
-  'Guinea-Bissau',
-  'Guyana',
-  'Haiti',
-  'Honduras',
-  'Hungary',
-  'Iceland',
-  'India',
-  'Indonesia',
-  'Iran',
-  'Iraq',
-  'Ireland',
-  'Israel',
-  'Italy',
-  'Jamaica',
-  'Japan',
-  'Jordan',
-  'Kazakhstan',
-  'Kenya',
-  'Kiribati',
-  'Korea, North',
-  'Korea, South',
-  'Kuwait',
-  'Kyrgyzstan',
-  'Laos',
-  'Latvia',
-  'Lebanon',
-  'Lesotho',
-  'Liberia',
-  'Libya',
-  'Liechtenstein',
-  'Lithuania',
-  'Luxembourg',
-  'Madagascar',
-  'Malawi',
-  'Malaysia',
-  'Maldives',
-  'Mali',
-  'Malta',
-  'Marshall Islands',
-  'Mauritania',
-  'Mauritius',
-  'Mexico',
-  'Micronesia',
-  'Moldova',
-  'Monaco',
-  'Mongolia',
-  'Montenegro',
-  'Morocco',
-  'Mozambique',
-  'Myanmar',
-  'Namibia',
-  'Nauru',
-  'Nepal',
-  'Netherlands',
-  'New Zealand',
-  'Nicaragua',
-  'Niger',
-  'Nigeria',
-  'North Macedonia',
-  'Norway',
-  'Oman',
-  'Pakistan',
-  'Palau',
-  'Palestine',
-  'Panama',
-  'Papua New Guinea',
-  'Paraguay',
-  'Peru',
-  'Philippines',
-  'Poland',
-  'Portugal',
-  'Qatar',
-  'Romania',
-  'Russia',
-  'Rwanda',
-  'Saint Kitts and Nevis',
-  'Saint Lucia',
-  'Saint Vincent and the Grenadines',
-  'Samoa',
-  'San Marino',
-  'Sao Tome and Principe',
-  'Saudi Arabia',
-  'Senegal',
-  'Serbia',
-  'Seychelles',
-  'Sierra Leone',
-  'Singapore',
-  'Slovakia',
-  'Slovenia',
-  'Solomon Islands',
-  'Somalia',
-  'South Africa',
-  'South Sudan',
-  'Spain',
-  'Sri Lanka',
-  'Sudan',
-  'Suriname',
-  'Sweden',
-  'Switzerland',
-  'Syria',
-  'Tajikistan',
-  'Tanzania',
-  'Thailand',
-  'Togo',
-  'Tonga',
-  'Trinidad and Tobago',
-  'Tunisia',
-  'Turkey',
-  'Turkmenistan',
-  'Tuvalu',
-  'Uganda',
-  'Ukraine',
-  'United Arab Emirates',
-  'United Kingdom',
-  'United States',
-  'Uruguay',
-  'Uzbekistan',
-  'Vanuatu',
-  'Vatican City',
-  'Venezuela',
-  'Vietnam',
-  'Yemen',
-  'Zambia',
-  'Zimbabwe'
-]
-for countr in countries:
-    if 'land' in countr:
+from data.countries import countries
+print(f'imported {len(countries)} country-data')
+for countr in countries:#durch alle Länder iterieren
+    if 'land' in countr:#String in String suchen
         print(countr,end = '-')
 print()
 #Liste reverse mit for
@@ -309,38 +121,33 @@ for fruit in range(len(fruits)-1,-1,-1):#Wert Ende ist exclusive!
     print(fruits[fruit])
 
 #Liste aus Datei importieren, Ordner data ist zwei Ebenen über script
-Pfad=__file__ #relativer oder absoluter Pfad zum Script
-Pfad=os.path.abspath(Pfad) #in absoluten Pfad wandeln
-DirPfad=os.path.dirname(Pfad)#Filenamen abtrennen
-ParentDirPfad=os.path.join(DirPfad,'..','..')#Zwei Verzeichnisebenen nach oben anhängen
-ParentDirPath=os.path.normpath(ParentDirPfad)#evtl in Normpfad wandeln, nicht unb. nötig
-print(f'Pfad: {ParentDirPath}')
-sys.path.append(ParentDirPath)# Pfad nun in Path aufnehmen
+
 from data.countries_working_file import countries_data#Liste aus Modul (Datei) importieren
 print(f'Einträge in countries_data: {len(countries_data)}')
 #Zählen wie oft jede Sprache vorkommt, in dict ablegen
-dictLanguages={'English':0}#Dictionary erzeugen
-for nDict in countries_data:#durch alle Dicts in Liste iterieren
-    for lang in nDict['languages']:#durch Liste der Sprachen im dict iterieren
-        print(lang)
-        if lang in dictLanguages:   #wenn Sprache schon in dict
+dictLanguages={'English':0}#Dictionary erzeugen mit Sprache und Anzahl vorkommen
+for nDict in countries_data:#durch alle Dicts in Liste countries_data iterieren
+    for lang in nDict['languages']:#durch Liste der Sprachen im aktuellen nDict iterieren
+        #print(lang)
+        if lang in dictLanguages:   #wenn Sprache schon in dictLanguages ist
             y=dictLanguages[lang]   #aktuelle Anzahl auslesen
             y+=1    #erhöhen
             dictLanguages[lang]=y   #wieder speichern
-        else:#Sprache noch nicht in dict
-            dictLanguages[lang]=1   #Eintrag anlegen und auf 1 setzen
+        else:#Sprache noch nicht in dictLanguages
+            dictLanguages[lang]=1   #Eintrag Schlüssel 'z.B.Pashtu' anlegen und auf 1 setzen
 print(f'Anzahl Sprachen: {len(dictLanguages)}')
+#10 häufigste Sprachen ermitteln aus dictLanguages und Anzeigen
 for i in range(10):#10 Durchläufe, 10 häufigste Sprachen anzeigen
     count_langu=1#Zähler zurücksetzen
-    name_langu=''
-    for lang in dictLanguages:# Sprachen iterieren
+    name_langu='' #Name der Sprache
+    for lang in dictLanguages:# durch alle Sprachen im dict iterieren
         if dictLanguages[lang] > count_langu:#Anzahl höher ?
             count_langu = dictLanguages[lang]#neue Höchstzahl speichern
             name_langu=lang#Name dazu speichern
     print(f'Sprache: {name_langu} Anzahl: {count_langu}')#Aktuelle Höchstzahl anzeigen
-    dictLanguages.pop(name_langu) #Sprache entfernen vor neuem Durchlauf 
+    dictLanguages.pop(name_langu) #Sprache mit höchstzahl entfernen vor neuem Durchlauf 
 
-dict_popu={} #neues Dictionary anlegen mit Land:Bevölkerung
+dict_popu={} #neues Dictionary anlegen mit 'Land':Bevölkerung
 for nDict in countries_data: #durch alle dicts in Liste iterieren
     #neues Dictionary mit Werten aus Dictionaries in Liste füllen, Name und Einwohner
     dict_popu[nDict.get('name')] = nDict.get('population')#Dictionaryeintrag anlegen
